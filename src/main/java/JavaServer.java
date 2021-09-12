dac6cf863c1f8fde660fa7015826ab7ae8c77673
Lesco Andrei <53511833+whysoserious97@users.noreply.github.com>
2021-09-12 09:40:47 +0300

    DataBaseHandler added

--- src/main/java/JavaServer.java
+++ src/main/java/JavaServer.java
@@ -2,17 +2,57 @@ import org.apache.xmlrpc.*;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
 import org.apache.xmlrpc.webserver.WebServer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class JavaServer {
+
+    static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/microservice_login","root","root");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+    public JavaServer() throws SQLException {
+    }
 
     public Integer sum(int x, int y){
         return new Integer(x+y);
     }
 
-    public static void main (String [] args){
 
+    public Boolean register(String login,String password){
+        String response = "";
         try {
+            response =  DataBaseHandler.register(login,password);
+        }catch (Exception e){ e.printStackTrace(); }
 
-            System.out.println("Attempting to start XML-RPC Server...");
+        return response.equals("Success");
+    }
+
+
+    public String login(String login,String password)  {
+        String response = "";
+        try {
+           response =  DataBaseHandler.login(login,password);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        if (response.equals("Success")){
+            return "Authorized";
+        }
+        return "Failed to connect";
+    }
+
+
+    public static void main (String [] args){
+        try {
 
             WebServer server = new WebServer(8800);
 
@@ -23,7 +63,8 @@ public class JavaServer {
             server.start();
 
             System.out.println("Started successfully.");
-            System.out.println("Accepting requests. (Halt program to stop.)");
+
+            DataBaseHandler.init(connection);
 
         } catch (Exception exception){
             System.err.println("JavaServer: " + exception);
